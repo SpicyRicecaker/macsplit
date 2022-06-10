43bf912419e5ad2ecc858d92d5b644f10def9961
Andy Li <54951411+SpicyRicecaker@users.noreply.github.com>
2022-06-10 13:17:47 -0700

    Initial Commit

---
+++ macsplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  macsplit
+//
+//  Created by Oliver LI on 6/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ macsplit/macsplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  macsplitApp.swift
+//  macsplit
+//
+//  Created by Oliver LI on 6/10/22.
+//
+
+import SwiftUI
+
+@main
+struct macsplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
